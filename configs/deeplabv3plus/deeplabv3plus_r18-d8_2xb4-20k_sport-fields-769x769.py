65bb198365775cb3601aa4aacea95750d62926f3
Michael Gruner <michael.gruner@ridgerun.ai>
2023-09-08 03:55:11 +0000

    Fix wrong path in config sport-fields config files

--- configs/deeplabv3plus/deeplabv3plus_r18-d8_2xb4-20k_sport-fields-769x769.py
+++ configs/deeplabv3plus/deeplabv3plus_r18-d8_2xb4-20k_sport-fields-769x769.py
@@ -1,4 +1,4 @@
-_base_ = './deeplabv3plus_r50-d8_2xb4-80k_sport-fields-769x769.py'
+_base_ = './deeplabv3plus_r50-d8_2xb4-20k_sport-fields-769x769.py'
 model = dict(
     pretrained='open-mmlab://resnet18_v1c',
     backbone=dict(depth=18),
